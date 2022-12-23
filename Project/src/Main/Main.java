276e674a69e9940708b2a1404a4a989e512b59ae
Asser <assertmaer2002@gmail.com>
2022-12-24 00:22:02 +0200

    ebtdet tnada3

--- Project/src/Main/Main.java
+++ Project/src/Main/Main.java
@@ -3,21 +3,11 @@ package Main;
 import eg.edu.alexu.csd.oop.game.GameEngine;
 import view.Game;
 
-/**
- *
- * @author omari
- */
-public class Main{
-    
-public static void main(String[] args) {
-    
-    GameEngine.start("Very Simple Game in 99 Line of Code", new Game(600, 600));
-    System.out.println("Hello world");
-    
-    
-    
-        
-}
-}
+public class Main {
 
+    public static void main(String[] args) {
 
+        GameEngine.start("Test Game", new Game(600, 600));
+
+    }
+}
--- Project/src/model/PlateObject.java
+++ Project/src/model/PlateObject.java
@@ -1,29 +1,66 @@
 package model;
 
+import eg.edu.alexu.csd.oop.game.GameObject;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class PlateObject extends Shapes {
+public class PlateObject implements Shapes,GameObject {
 
-    private int x,y;
+    private int x, y;
     private static final int MAX_MSTATE = 1;
-    private String picpath;
+    boolean visible;
     private BufferedImage[] spriteImages = new BufferedImage[MAX_MSTATE];
 
-    public PlateObject(int posX, int posY, String path){
-		this.x = posX;
-		this.y = posY;
-		this.visible = true;
-		try {
-			spriteImages[0] = ImageIO.read(new File(path));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public PlateObject(int posX, int posY, String path) {
+        this.x = posX;
+        this.y = posY;
+        this.visible = true;
+        try {
+            spriteImages[0] = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public BufferedImage[] getSpriteImages() {
         return spriteImages;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public int getWidth() {
+        return this.spriteImages[0].getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return this.spriteImages[0].getHeight();
+    }
+    
+    @Override
+    public boolean isVisible() {
+        return visible;
     }
 }
