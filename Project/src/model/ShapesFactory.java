a3f4fbbc35ff8bd6504d3a64cd8a296224b18880
Iraqy <omariraqy16@gmail.com>
2022-12-24 15:06:58 +0200

    Image not working

--- Project/src/model/ShapesFactory.java
+++ Project/src/model/ShapesFactory.java
@@ -5,15 +5,12 @@ public class ShapesFactory {
 
     public Shapes getShape(int width,int height, String shapeName) {
         if (shapeName == "GreenPlate") {
-<<<<<<< HEAD
             return new PlateObject(20, 10, "C:/Users/omari/Project/Project/src/resources/greenplatewithbase.png");
         } else if (shapeName == "RedPlate") {
             return new PlateObject(100, 10, "C:/Users/omari/Project/Project/src/resources/redplatewithbase.png");
-=======
             return new PlateObject((int) (Math.random() * (width/2)), -1 * (int) (Math.random() * height), "./images/greenplatewithbase.png",1);
         } else if (shapeName == "RedPlate") {
             return new PlateObject((int) (Math.random() *(width/2)), -1 * (int) (Math.random() * height), "./images/redplatewithbase.png",2);
->>>>>>> Asser
         }
         return null;
     }
